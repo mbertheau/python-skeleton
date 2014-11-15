b321c074ddc52cbc15a95d7cf6f9db7185c56ce6
Markus Bertheau <mbertheau@gmail.com>
2014-11-15 21:05:21 +0100

    Correct indentation

--- setup.py
+++ setup.py
@@ -31,17 +31,17 @@ requires = [
 ]
 
 classifiers = [
-        'Development Status :: 4 - Beta',
-        'Environment :: Web Environment',
-        'Framework :: Django',
-        'Intended Audience :: Developers',
-        'License :: OSI Approved :: MIT License',
-        'Operating System :: OS Independent',
-        'Programming Language :: Python',
-        'Programming Language :: Python :: 2.6',
-        'Programming Language :: Python :: 2.7',
-        'Topic :: Software Development :: Debuggers',
-        'Topic :: Software Development :: Libraries :: Python Modules',
+    'Development Status :: 4 - Beta',
+    'Environment :: Web Environment',
+    'Framework :: Django',
+    'Intended Audience :: Developers',
+    'License :: OSI Approved :: MIT License',
+    'Operating System :: OS Independent',
+    'Programming Language :: Python',
+    'Programming Language :: Python :: 2.6',
+    'Programming Language :: Python :: 2.7',
+    'Topic :: Software Development :: Debuggers',
+    'Topic :: Software Development :: Libraries :: Python Modules',
 ]
 
 setup(
